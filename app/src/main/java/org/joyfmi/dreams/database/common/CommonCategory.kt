cdcb9d1ac8132b6c34285d50d9eea0d06cee49a6
Chris Kottaridis <chriskot@quietwind.net>
2022-02-28 19:03:22 -0600

    Make the Name fileds in Common Database unique
    We want to make sure that the names of the categorys and symbols
    of the Common Database are unique. THis is done in SQLite with
    Indexes.
    The following commands were done in the database since the indexes
    were not created at creation time:
    CREATE UNIQUE INDEX symbol_name ON Symbol(Name)
    CREATE UNIQUE INDEX category_name ON Category(Name)

--- app/src/main/java/org/joyfmi/dreams/database/common/CommonCategory.kt
+++ app/src/main/java/org/joyfmi/dreams/database/common/CommonCategory.kt
@@ -3,12 +3,13 @@ package org.joyfmi.dreams.database.common
 import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 /*
  * Define the data Category class that matches the Category Table of the database
  */
-@Entity
+@Entity(indices = [Index(value = ["Name"], unique = true)])
 data class CommonCategory (
     /*
      * The definitions and types here should match the database Schema for the Category table
--- app/src/main/java/org/joyfmi/dreams/database/common/CommonSymbol.kt
+++ app/src/main/java/org/joyfmi/dreams/database/common/CommonSymbol.kt
@@ -3,12 +3,13 @@ package org.joyfmi.dreams.database.common
 import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 /*
  * Define the data Symbol class that matches the Symbol Table of the database
  */
-@Entity
+@Entity(indices = [Index(value = ["Name"], unique = true)])
 data class CommonSymbol (
     /*
      * The definitions and types here should match the database Schema for the Symbol table
