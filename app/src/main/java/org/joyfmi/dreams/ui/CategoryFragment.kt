3b777ae6c63bddd028c9e77650a135b967c0fa1a
Chris Kottaridis <chriskot@quietwind.net>
2022-03-15 00:11:02 -0500

    Add code for AutoComplete to go from Smbol Frag to Meaning Frag
    Had to implement the AdapterView.OnItemClickListener in the
    Category Fragment as well as the Fragment Class.

--- app/src/main/java/org/joyfmi/dreams/ui/CategoryFragment.kt
+++ app/src/main/java/org/joyfmi/dreams/ui/CategoryFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
@@ -19,7 +20,17 @@ import org.joyfmi.dreams.DreamApplication
 import org.joyfmi.dreams.R
 import org.joyfmi.dreams.repository.SymbolIdentity
 
-class CategoryFragment: Fragment() {
+/*
+ * We define a Fragment for the Category page to be of class Fragment.
+ * We also want CategoryFragment to implement the AdapterView.OnItemClickListener interface.
+ * The comma after Fragment() indicates this. The OnItemClickListener is needed to notice
+ * the Symbol AutoComplete. For the AdapterView.OnItemClickListener interface we need to override
+ * it's onItemClick() function. By adding this function we can use this class as the
+ * onItemClickListener for the SymbolNameView. When an item on the list is clicked this class'
+ * onItemClick will get control and can use the navigation info to go to the SymbolFragment and
+ * provide the symbolIdentity.
+ */
+class CategoryFragment: Fragment(), AdapterView.OnItemClickListener {
 
     private var _binding: CategoryFragmentBinding? = null
 
@@ -72,6 +83,8 @@ class CategoryFragment: Fragment() {
             R.id.symbol_name_view)
             //mutableListOf<SymbolIdentity>())
 
+        Log.d("SymbolName Action", "setting SymbolName  Listener")
+        symbolNameView.onItemClickListener = this
         symbolNameView.setAdapter(symbolNameAdapter)
         viewModel.loadSymbolList(symbolNameAdapter)
     }
@@ -80,4 +93,29 @@ class CategoryFragment: Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    /*
+     * This function is needed to implement the AdapterView.OnItemClickListener so that this
+     * class can be used as the SymbolNameView's onItemClickListener.
+     */
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        /*
+         * get the value clicked on from it's position value in the parent AdapterView
+         */
+        val identity = parent?.getItemAtPosition(pos)
+        /*
+         * Define the action from the Navigator control info that takes us to the Meaning Fragment.
+         * Pass the identity as the symbolIdentity value to the Meaning Fragment
+         */
+        val action = CategoryFragmentDirections.actionCategoryFragmentToMeaningFragment(
+            symbolIdentity = identity as SymbolIdentity
+        )
+        /*
+         * The view that has the navigation info on how to get to the Meaning Fragment is in
+         * the binding.root value. So, pass it the action to get to the Meaning Fragment
+         */
+        val categoryView = binding.root
+        categoryView.findNavController().navigate(action)
+    }
+
 }
